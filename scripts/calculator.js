49a2f5533edf5b4f5f0f39939a8be9333bb52ec0
Halil Bilgil <hbilgil@gmail.com>
2022-08-17 14:12:47 +0300

    Edit calculator.js to define 4 functions

---
+++ scripts/calculator.js
@@ -0,0 +1,25 @@
+const add = function(a, b) {
+    return +a + +b;
+};
+  
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const divide = function (a, b) {
+    if(a === 0 && b === 0 || a !== 0 && b === 0) return "undefined";
+    return a / b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const calculator = {
+    add,
+    subtract,
+    divide,
+    multiply,
+};
+
+module.exports = calculator;
